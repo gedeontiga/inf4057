968969691ef1ae325cde27439607306d8babf6e3
brunel237 <bruneltsafack@gmail.com>
2024-11-12 15:24:47 +0100

    commit 1

---
+++ service-bank/src/main/java/com/m1fonda/service_bank/ServiceBankApplication.java
@@ -0,0 +1,13 @@
+package com.m1fonda.service_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBankApplication.class, args);
+	}
+
+}
---
+++ service-transactions/src/test/java/com/m1fonda/service_transactions/ServiceTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m1fonda.service_transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
