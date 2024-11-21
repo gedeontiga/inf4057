d35300bb0d7fc6b99f2be8a2649666e6fd6560f0
brunel237 <bruneltsafack@gmail.com>
2024-11-21 11:53:37 +0100

    last commit

--- service-bank/src/main/java/com/m1fonda/service_bank/service/BankService.java
+++ service-bank/src/main/java/com/m1fonda/service_bank/service/BankService.java
@@ -1,6 +1,5 @@
 package com.m1fonda.service_bank.service;
 
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
